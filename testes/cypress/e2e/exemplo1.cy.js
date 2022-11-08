0fc5fed5a4f150929868469f5e0afde8b6d89310
GustavoFLuz <gu.luz400@gmail.com>
2022-11-08 17:30:38 -0300

    Trabalho/aula 4

--- testes/cypress/e2e/exemplo1.cy.js
+++ testes/cypress/e2e/exemplo1.cy.js
@@ -1,55 +1,51 @@
 /// <reference types='cypress'/>
 
-describe('Criando cenário de teste para o site globalsqa', () => {
-
-  it.skip('Caso de teste: Registrando um usuário no site com sucesso', () => {
-    let userInfo = createUser();
-
-    cy.get('.ng-binding').should('contain.text', 'Registration successful')
+describe('Criando cenário de teste para a documentação do Cypress', () => {
+  it('Caso de teste: Entrar no item "API" da barra de navegação', () => {
+    visit();
+    cy.get('.flex > [href="/api/table-of-contents"]').click()
+    cy.get('h1').should('contain.text', 'Table of Contents') 
   })
 
-  it.skip('Caso de teste: Registrando um usuário no site com falha (faltando senha)', () => {
-    cy.visit('https://globalsqa.com/angularJs-protractor/registration-login-example/#/register')
-
-    cy.get('#firstName').type('Inatel');
-    cy.get('#Text1').type('Inatel');
-    cy.get('#username').type('Inatel');
-    cy.get('#password').type('Inatel');
-
-    cy.get('#password').clear();
-
-    cy.get('.has-error > .help-block').should('have.text', 'Password is required')
-    cy.get('.btn-primary').should('be.disabled');
+  it('Caso de teste: pesquisar "Writing Your First E2E Test"', () => {
+    visit();
+    cy.get('.DocSearch').click()
+    cy.get('#docsearch-input').type('Writing Your First E2E Test')
+    cy.get('#docsearch-item-0').click();
+    cy.get('h1').should('contain.text', 'Writing Your First E2E Test') 
   })
-
-  it('Caso de teste: Registrando um login no site com sucesso', () => {
-    let userInfo = createUser();
-
-    cy.get('#username').type(userInfo.user);
-    cy.get('#password').type(userInfo.password);
-
-    cy.get('.btn-primary').click();
-
-    cy.get('h1.ng-binding').should('contain.text', `Hi ${userInfo.user}!` )
+  it('Caso de teste: botão "Back to Top"', () => {
+    visit();
+    cy.scrollTo('bottom')
+    cy.get('.text-blue').click()
+    cy.window().its('scrollY').should('equal', 0)
+  })
+  it('Caso de teste: navegação para learn cypress', () => {
+    visit();
+    cy.get('.flex > [href="https://learn.cypress.io"]').click()
+    cy.url().should('include', 'https://learn.cypress.io')
+  })
+  it('Caso de teste: dropdown do menu esquerdo não deve alterar o conteúdo', () => {
+    visit();
+    cy.get('h1').then(h1Before=>{
+      const text = h1Before.text();
+      cy.get('[data-test="dashboard"] > .text-left').click()
+      cy.get('h1').should(h1After => {
+        expect(h1After.text()).to.eq(text)
+      }) 
+    })
+  })
+  it('Caso de teste: clicar no menu direito não deve permanecer na mesma página', () => {
+    visit();
+    cy.url().then(oldUrl=>{
+      cy.get('.scrollactive-nav > :nth-child(11) > .block').click();
+      cy.url().then(newUrl=>{
+        expect(newUrl!=oldUrl)
+      })
+    })
   })
 })
 
-function createUser() {
-
-  let hours  = new Date().getHours() + ':' + new Date().getMinutes() + ':' + new Date().getSeconds();
-  let time =  new Date().getTime();
-
-  cy.visit('https://globalsqa.com/angularJs-protractor/registration-login-example/#/login')
-
-  cy.get('.btn-link').click();
-
-  cy.get('#firstName').type(hours);
-  cy.get('#Text1').type(hours);
-  cy.get('#username').type(hours);
-  cy.get('#password').type(time);
-
-  cy.get('.btn-primary').click();
-  cy.get('.ng-binding').should('contain.text', 'Registration successful')
-
-  return {user:hours, password:time}
+function visit() {
+  cy.visit('https://docs.cypress.io')
 }
